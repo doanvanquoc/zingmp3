eb00e98bbc2365cca73563678bfe79d2482430cb
doanvanquoc <vanquoc.dev@gmail.com>
2024-03-05 17:55:26 +0700

    đổi base url

--- lib/services/api_routes.dart
+++ lib/services/api_routes.dart
@@ -1,5 +1,5 @@
 class ApiRoutes {
-  static const String baseUrl = 'https://zingmp3.vanquoc.id.vn/api/';
+  static const String baseUrl = 'https://zingmp3.vanquoc.me/api/';
   static const String getHome = 'home';
   static const String getMusicLink = 'song?id={id}';
   static const String getPlaylistDetail = 'detailplaylist?id={id}';
